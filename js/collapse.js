41a4d92f0754e399924982bd2b49bf8428b43254
Ben Price <bprice@friendsbalt.org>
2019-08-13 16:26:16 -0400

    added a toggle to expand and collapse

--- js/collapse.js
+++ js/collapse.js
@@ -2,10 +2,11 @@ let buttons = document.getElementsByClassName("expand");
 let bios = document.getElementsByClassName("member-bio");
 
 expand = function() {
-  bios[this.id].classList.remove("collapsed");
+  bios[this.id].classList.toggle("collapsed");
 }
 
 for (var i = 0, len = buttons.length; i < len; i++) {
+  buttons[i].isCollapsed = true;
   buttons[i].id = i;
   buttons[i].addEventListener('click', expand);
   }
