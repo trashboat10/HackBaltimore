d153612a7ba683f3fbf9f0f022d912c5c9dfa889
Ben Price <bprice@friendsbalt.org>
2019-08-13 17:04:42 -0400

    text on button changes when pressed

--- js/collapse.js
+++ js/collapse.js
@@ -3,10 +3,15 @@ let bios = document.getElementsByClassName("member-bio");
 
 expand = function() {
   bios[this.id].classList.toggle("collapsed");
+  if (bios[this.id].classList.contains("collapsed")) {
+    this.innerHTML = "expand";
+  } else {
+    this.innerHTML = "collapse";
+  }
+
 }
 
 for (var i = 0, len = buttons.length; i < len; i++) {
-  buttons[i].isCollapsed = true;
   buttons[i].id = i;
   buttons[i].addEventListener('click', expand);
   }
