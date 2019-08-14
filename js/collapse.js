6ae0a17e297194d686a66de3baf7ac0d2869d3b1
Ben Price <bprice@friendsbalt.org>
2019-08-14 10:50:49 -0400

    made expanding and collapsing affect the whole page

--- js/collapse.js
+++ js/collapse.js
@@ -2,13 +2,14 @@ let buttons = document.getElementsByClassName("expand");
 let bios = document.getElementsByClassName("member-bio");
 
 expand = function() {
-  bios[this.id].classList.toggle("collapsed");
-  if (bios[this.id].classList.contains("collapsed")) {
-    this.innerHTML = "expand";
-  } else {
-    this.innerHTML = "collapse";
+  for (var i = 0; i < bios.length; i++){
+    bios[i].classList.toggle("collapsed");
+    if (bios[i].classList.contains("collapsed")) {
+      buttons[i].innerHTML = "expand";
+    } else {
+      buttons[i].innerHTML = "collapse";
+    }
   }
-
 }
 
 for (var i = 0, len = buttons.length; i < len; i++) {
